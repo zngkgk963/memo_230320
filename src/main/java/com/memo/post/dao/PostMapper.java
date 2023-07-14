56aae786f56e4c8ceabfa62067e55b813aed5275
test <test@gamil.com>
2023-07-14 16:35:35 +0900

    초기 환경 세팅

---
+++ src/main/java/com/memo/post/dao/PostMapper.java
@@ -0,0 +1,12 @@
+package com.memo.post.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+// MyBatis
+@Repository
+public interface PostMapper {
+	public List<Map<String, Object>> selectPostList();
+}
