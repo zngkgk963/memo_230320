5310e6bb260cf45aaddc45d4907dfdea598771e0
test <test@gamil.com>
2023-08-07 18:42:03 +0900

    학원

--- src/test/java/com/memo/MemoApplicationTests.java
+++ src/test/java/com/memo/MemoApplicationTests.java
@@ -1,13 +1,32 @@
 package com.memo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MemoApplicationTests {
-
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Test
 	void contextLoads() {
+		
 	}
-
+	
+	
+	@Test
+	void 더하기테스트() {
+		logger.debug("######## 더하기 테스트 시작");
+		int a = 10;
+		int b = 20;
+		
+		assertEquals(30, 더하기(a, b));
+	}
+	
+	int 더하기(int x, int y) {
+		return x + y;
+	}
 }
